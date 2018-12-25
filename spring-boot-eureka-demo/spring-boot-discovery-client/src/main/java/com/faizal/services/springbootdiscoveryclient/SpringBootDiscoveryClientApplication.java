48ced1ee00d6c1b4332d79f891afec0a89d02804
Faizal Zafri <zafri_faizal.outlook.com>
2018-12-25 23:20:39 +0530

    spring-boot-eureka

---
+++ spring-boot-eureka-demo/spring-boot-discovery-client/src/main/java/com/faizal/services/springbootdiscoveryclient/SpringBootDiscoveryClientApplication.java
@@ -0,0 +1,14 @@
+package com.faizal.services.springbootdiscoveryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDiscoveryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDiscoveryClientApplication.class, args);
+	}
+
+}
+
---
+++ spring-boot-eureka-demo/spring-boot-discovery/src/main/java/com/faizal/services/springbootdiscovery/SpringBootDiscoveryApplication.java
@@ -0,0 +1,14 @@
+package com.faizal.services.springbootdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDiscoveryApplication.class, args);
+	}
+
+}
+
