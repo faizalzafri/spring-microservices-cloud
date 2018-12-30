80a65db36a0db7e0d9fb69bd0281dfcc2ff66eab
Faizal Zafri <zafri_faizal.outlook.com>
2018-12-31 02:10:46 +0530

    spring-boot-eureka

--- spring-boot-eureka-demo/spring-boot-discovery-client/src/main/java/com/faizal/services/springbootdiscoveryclient/SpringBootDiscoveryClientApplication.java
+++ spring-boot-eureka-demo/spring-boot-discovery-client/src/main/java/com/faizal/services/springbootdiscoveryclient/SpringBootDiscoveryClientApplication.java
@@ -2,8 +2,10 @@ package com.faizal.services.springbootdiscoveryclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringBootDiscoveryClientApplication {
 
 	public static void main(String[] args) {
---
+++ spring-boot-eureka-demo/spring-boot-discovery-client/src/main/java/com/faizal/services/springbootdiscoveryclient/controller/ClientController.java
@@ -0,0 +1,18 @@
+package com.faizal.services.springbootdiscoveryclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientController {
+
+	//@Value("${spring.profiles}")
+	private String zone;
+
+	@GetMapping("/ping")
+	public String ping() {
+		return "I'm in zone " ;
+	}
+
+}
