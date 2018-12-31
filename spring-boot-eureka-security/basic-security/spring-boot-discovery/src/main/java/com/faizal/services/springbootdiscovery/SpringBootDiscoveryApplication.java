366d891f49feb16a593587b46d97a0c6a01e324d
Faizal Zafri <zafri_faizal.outlook.com>
2018-12-31 13:11:22 +0530

    spring-boot-eureka-security

---
+++ spring-boot-eureka-security/basic-security/spring-boot-discovery/src/main/java/com/faizal/services/springbootdiscovery/SpringBootDiscoveryApplication.java
@@ -0,0 +1,16 @@
+package com.faizal.services.springbootdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringBootDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDiscoveryApplication.class, args);
+	}
+
+}
+
